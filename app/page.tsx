aff020a827ab113c35099a516643022008f1e44d
Med Ali Khaled <medalikhaled331@gmail.com>
2023-12-06 17:05:13 +0100

    implementing dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ui/toogle-mode";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      hello
+      <Button>hello</Button>
+      <ModeToggle />
     </main>
   );
 }
